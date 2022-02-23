ede6c596492db6910e32f79b6a85183e6467dfab
devdsml <95732612+devdsml@users.noreply.github.com>
2022-02-23 21:44:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,27 @@ import dash
 import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
+import pandas as pd
+import plotly.express as px
 from dash.dependencies import Input, Output
  
 USERNAME_PASSWORD_PAIRS = [
-    ['nethu', '12345'],['guvi', 'guvi']
+    ['tstn','testn'],['guvi', 'guvi']
 ]
  
 app = dash.Dash()
 auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
 server = app.server
- 
-app.layout = html.Div([
+df = pd.read_csv('https://raw.githubusercontent.com/nethajinirmal13/Training-datasets/main/matches.csv')
+df.dropna(subset=['winner'], inplace=True)
+fig1 = px.treemap(df, path=['winner'])
+fig1.update_traces(textinfo = 'label + value')
+
+app.layout = html.Div(children=[
+    dcc.Graph(
+       id='winteam',
+       figure=fig1
+    ),
     dcc.RangeSlider(
         id='range-slider',
         min=-5,
